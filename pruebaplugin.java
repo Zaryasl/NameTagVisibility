76ff044fed0a7b9e8e89c0f9607aa0df30d5ffcc
Zarya <149230745+Zaryasl@users.noreply.github.com>
2023-10-28 19:39:36 -0400

    Update pruebaplugin.java

--- pruebaplugin.java
+++ pruebaplugin.java
@@ -1,5 +1,8 @@
 package pruebaplugin;
 
+import org.bukkit.event.Listener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,6 +10,9 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Team;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.Location;
+import java.util.List;
 
 public class pruebaplugin extends JavaPlugin implements Listener {
 
@@ -27,14 +33,14 @@ public class pruebaplugin extends JavaPlugin implements Listener {
         team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
     }
 
-    public static void main(String[] args) {
-        // No necesitas implementar nada aquí para un complemento de Spigot.
-        // Este método es solo para aplicaciones Java independientes.
-        class pruebaplugin {
-
-            void addPlayer(@NotNull Player player) {
-                // Código para añadir un jugador
-
+    public List<String> tabComplete(CommandSender sender, String alias, String[] args, Location location) throws IllegalArgumentException {
+        if (sender == null || alias == null || args == null) {
+            throw new IllegalArgumentException("Argumentos no pueden ser nulos");
+        }
+        // Resto del código
+        return null; // Cambia esto según sea necesario
+    }
+}
 
 
             }
